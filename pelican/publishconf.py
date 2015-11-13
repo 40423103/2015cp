818093d67a18664e5c31315d57579545751da373
40423103 <40423103@gm.nfu.edu.tw>
2015-11-13 11:56:26 +0800

    reveal

--- pelican/publishconf.py
+++ pelican/publishconf.py
@@ -7,8 +7,8 @@ from __future__ import unicode_literals
 
 import os
 
-AUTHOR = 'KMOL'
-SITENAME = '2015FALL KMOL 課程 __ 40423103個人倉儲作業'
+AUTHOR = '劉怡萱'
+SITENAME = '2015FALL KMOL 課程 __ 40423103個人作業'
 SITEURL = ''
 
 PATH = 'content'
